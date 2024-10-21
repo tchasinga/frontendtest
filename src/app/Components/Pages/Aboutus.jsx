8c5a205acf1c3b6bc1a82f2d737751d86190c52c
Tchasinga jacques <tchasingajacques@gmail.com>
2024-10-21 18:13:10 +0300

    Add Aboutus component and integrate it into the Home page

---
+++ src/app/Components/Pages/Aboutus.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Image from 'next/image'
+
+export default function Aboutus() {
+  return (
+    <div className=' p-3 max-w-4xl mx-auto'>
+        
+        <div className="">
+            {/* first flex box side */}
+            <div className="relative">
+                
+            </div>
+        </div>
+
+    </div>
+  )
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
+import Aboutus from "./Components/Pages/Aboutus";
 import Heros from "./Components/Pages/Heros";
 
 export default function Home() {
   return (
     <div className="">
       <Heros />
+      <Aboutus />
     </div>
   );
 }
