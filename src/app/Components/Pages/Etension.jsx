274b68e2364bb280c3c91ff61db88be408c93681
Tchasinga jacques <tchasingajacques@gmail.com>
2024-10-21 18:00:27 +0300

    Add Etension component with dynamic content from Firstdatacollection; update layout and styles

--- src/app/Components/Pages/Etension.jsx
+++ src/app/Components/Pages/Etension.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import Firstdatacollection from '../../Data/Firstdatacollection.js'
 
 export default function Etension() {
   return (
     <div>
-       <h1>Balolebwami asiwaju</h1>
+
+     <div className="">
+     <h1 className="text-[24px] text-white font-normal">Waarmee kunnen we je helpen?</h1>
+     </div>
+
+      <div className="flex justify-center items-center gap-4 w-full flex-wrap">
+        {
+          Firstdatacollection.map((items, index) => (
+            <div key={index} className="bg-white rounded-lg p-2  m-4">
+              <div className="flex items-center justify-start gap-2 w-[200px]">
+                  <div className="text-2xl">
+                    {items.icons}
+                  </div>
+
+                  <div className="text-sm">
+                    <p>{items.title}</p>
+                  </div>
+              </div>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
---
+++ src/app/Data/Firstdatacollection.js
@@ -0,0 +1,23 @@
+import { IoHammerOutline } from "react-icons/io5";
+import { IoShareOutline } from "react-icons/io5";
+import { IoChatbubblesOutline } from "react-icons/io5";
+
+const Firstdatacollection = [
+    {
+        id: 1,
+        title : "Reparatie melden",
+        icons : <IoHammerOutline />
+    },
+    {
+        id: 2,
+        title : "Huur betalen",
+        icons : <IoShareOutline />
+    },
+    {
+        id: 3,
+        title: "Contact",
+        icons: <IoChatbubblesOutline />
+    }
+]
+
+export default Firstdatacollection
