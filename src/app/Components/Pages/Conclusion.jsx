cd29b7addb6a793871e2953def5d6be100e3d231
Tchasinga jacques <tchasingajacques@gmail.com>
2024-10-22 08:32:01 +0300

    Fix layout issues in Conclusion component; update class names for better styling and responsiveness

--- src/app/Components/Pages/Conclusion.jsx
+++ src/app/Components/Pages/Conclusion.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Conclusion() {
   return (
-    <div className="max-w-7xl mx-auto w-full lg:h-[42px] md:h-[100%] flex flex-col justify-center items-center sm:h-[100%] bg-white">
-      <div className="flex items-center justify-between w-full">
+    <div className="max-w-7xl mx-auto w-full lg:h-[42px] md:h-[100%] flex flex-col justify-center items-center responsiveing  sm:h-[100%] bg-white">
+      <div className="flex items-center justify-between w-full flex-wrap">
 
         <div className="underline flex items-center justify-center gap-2 text-slate-900 text-sm font-medium">
             <p className="">Privacy</p>
