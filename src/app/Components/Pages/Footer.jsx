f5de497944a9ae39a7b42aecdd35e919f6f800b9
Tchasinga jacques <tchasingajacques@gmail.com>
2024-10-22 08:20:52 +0300

    Refactor Footer component layout for improved responsiveness; update class names for better styling

--- src/app/Components/Pages/Footer.jsx
+++ src/app/Components/Pages/Footer.jsx
@@ -5,7 +5,7 @@ import { FaLinkedinIn } from "react-icons/fa6";
 export default function Footer() {
   return (
     <div className='  lg:h-[236px] md:h-[100%] sm:h-[100%] bgFooter flex flex-col py-[2%] items-center justify-center'>
-      <div className="max-w-7xl mx-auto flex items-center justify-evenly w-full">
+      <div className="max-w-7xl mx-auto mygridtemplateTwoIng responsiveing  w-full">
 
           <div className="">
             <h1 className='text-slate-950 font-medium py-3'>Bel of mail ons</h1>   
